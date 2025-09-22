d6bdaac5ce8ab733c44111b4d0541db8c2b79d02
Stella <twin-stella@proton.me>
2025-09-22 14:53:43 +0300

    Running scripts from the update.d directory and default configs

--- doall.sh
+++ doall.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -e
 
 HERE="$(dirname "$(readlink -f "${0}")")"
 cd "$HERE"
 
 ./update.sh
+
+if [[ -d "./update.d" ]]; then
+    for script in ./update.d/*.sh; do
+        "$script"
+    done
+fi
+
 ./parse.sh
 ./process.sh
