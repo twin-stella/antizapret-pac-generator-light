aca05730a27709ce8cac1ede85d14230c1b40c11
Stella <twin-stella@proton.me>
2025-10-02 19:20:31 +0300

    Fix symlinks

--- update.d/51-fix_exclude_files.sh
+++ update.d/51-fix_exclude_files.sh
@@ -3,8 +3,8 @@ set -e
 
 for name in hosts ips
 do
-    include_file="config/include-${name}-custom.txt"
-    exclude_file="config/exclude-${name}-dist.txt"
+    include_file="$(readlink -f "config/include-${name}-custom.txt")"
+    exclude_file="$(readlink -f "config/exclude-${name}-dist.txt")"
 
     echo -n "Removes items from ${exclude_file} if they exists in ${include_file}..."
     grep -Fvxf "$include_file" "$exclude_file" > "${exclude_file}.tmp" && mv "${exclude_file}.tmp" "$exclude_file"
